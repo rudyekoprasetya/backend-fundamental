64ed356ebb735bafb8060d69c5e5c81d6c28aae2
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2021-12-18 11:14:47 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -32,6 +32,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const init = async() =>{
 	// instance collaborations
 	const collaborationsService = new CollaborationsService();
@@ -113,6 +118,13 @@ const init = async() =>{
 			  collaborationsService,
 			  notesService,
 			  validator: CollaborationsValidator,
+			},
+		},
+		{
+			plugin: _exports,
+			options: {
+			  service: ProducerService,
+			  validator: ExportsValidator,
 			},
 		},
 	]);
